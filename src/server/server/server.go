c65db41af5e279eae7c8bf7eb750a803bce6ca5b
pzsfeng <pinkeshzaveri@gmail.com>
2016-11-25 22:39:36 +0000

    add server

---
+++ src/server/server/server.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	pb "../rpcdef"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":50051"
+)
+
+// server is used to implement helloworld.GreeterServer.
+type server struct{}
+
+// SayHello implements helloworld.GreeterServer
+func (s *server) GetStatus(ctx context.Context,
+	in *pb.ServerSvcStatusRequest) (*pb.ServerSvcStatusResponse, error) {
+	return &pb.ServerSvcStatusResponse{Message: "Hello " + in.Name}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterServerSvcServer(s, &server{})
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
